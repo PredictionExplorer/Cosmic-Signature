40d5678f57db68d08974bbb994f7bc2ec7159ae1
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2023-02-17 19:30:45 -0600

    Add the initial bidding war contract

---
+++ contracts/biddingWar.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: CC0-1.0
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity ^0.8.18;
+
+contract BiddingWar is Ownable {
+
+    uint256 constant MILLION = 1000000;
+
+    // how much the currentBid increases after every bid
+    // we want 1%?
+    uint256 public priceIncrease = 1010000; // we are going to divide this number by a million
+
+    // how much the deadline is pushed after every bid
+    uint256 public secondsExtra = 3600;
+
+    // how much is the secondsExtra increased by after every bid
+    // 1.0001
+    uint256 public timeIncrease = 1000100;
+
+    uint256 public bidPrice = 10**15; // Price starts at .001 eth
+
+    address public lastBidder = address(0);
+
+    // After a withdrawal, start off the clock with this much time.
+    uint256 public initialSecondsUntilWithdrawal = 24 * 3600;
+
+    // The bid size will be 500 times smaller than the withdrawal amount initially
+    uint256 public initalBidAmountFraction = 500;
+
+    uint256 public withdrawalFraction = 2;
+
+    // when the money can be taken out
+    uint256 public withdrawalTime;
+
+    uint256 public numWithdrawals = 0;
+
+    mapping(uint256 => bytes32) public seeds;
+
+    event WithdrawalEvent(uint256 indexed withdrawalNum, address indexed destination, uint256 amount);
+    event BidEvent(address indexed lastBidder, uint256 bidPrice);
+    event DonationEvent(address indexed donator, uint256 amount);
+
+    // Entropy
+    bytes32 public entropy;
+
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function getBidPrice() public view returns (uint256) {
+        return (bidPrice * priceIncrease) / MILLION;
+    }
+
+    // send some ETH into the contract and affect nothing else.
+    function donate() public payable {
+        emit DonationEvent(_msgSender(), msg.value);
+    }
+
+    function updateEntropy() internal {
+        entropy = keccak256(abi.encode(
+            entropy,
+            block.timestamp,
+            blockhash(block.number),
+            lastBidder));
+    }
+
+    function bid() public payable {
+
+        if (lastBidder == address(0)) {
+            // someone just withdrew and we are starting from scratch
+            withdrawalTime = block.timestamp + initialSecondsUntilWithdrawal;
+            bidPrice = withdrawalAmount() / initalBidAmountFraction;
+        }
+
+        uint256 newBidPrice = getBidPrice();
+
+        require(
+            msg.value >= newBidPrice,
+            "The value submitted with this transaction is too low."
+        );
+
+        lastBidder = _msgSender();
+        bidPrice = newBidPrice;
+
+        // If someone bid after the deadline, make sure that 'secondsExtra' will be until the withdrawal
+        withdrawalTime = max(withdrawalTime, block.timestamp) + secondsExtra;
+        secondsExtra = (secondsExtra * timeIncrease) / MILLION;
+        if (msg.value > bidPrice) {
+            // Return the extra money to the bidder.
+            (bool success, ) = lastBidder.call{value: msg.value - bidPrice}("");
+            require(success, "Transfer failed.");
+        }
+
+        emit BidEvent(lastBidder, bidPrice);
+    }
+
+    receive() external payable {
+        bid();
+    }
+
+    function timeUntilWithdrawal() public view returns (uint256) {
+        if (withdrawalTime < block.timestamp) return 0;
+        return withdrawalTime - block.timestamp;
+    }
+
+    function withdrawalAmount() public view returns (uint256) {
+        return address(this).balance / withdrawalFraction;
+    }
+
+    function withdraw() public {
+        require(_msgSender() == lastBidder, "Only last bidder can withdraw.");
+        require(timeUntilWithdrawal() == 0, "Not enough time has elapsed.");
+
+        address destination = lastBidder;
+        lastBidder = address(0);
+
+        uint256 amount = withdrawalAmount();
+
+        updateEntropy();
+        seeds[numWithdrawals] = entropy;
+        numWithdrawals += 1;
+
+        (bool success, ) = destination.call{value: amount}("");
+        require(success, "Transfer failed.");
+
+        emit WithdrawalEvent(numWithdrawals - 1, destination, amount);
+    }
+
+    constructor() {
+        entropy = keccak256(abi.encode(
+            "Let's fight!",
+            block.timestamp, blockhash(block.number)));
+    }
+
+    function setTimeIncrease(uint256 newTimeIncrease) public onlyOwner {
+        timeIncrease = newTimeIncrease;
+    }
+
+    function setPriceIncrease(uint256 newPriceIncrease) public onlyOwner {
+        priceIncrease = newPriceIncrease;
+    }
+
+    function setSecondsExtra(uint256 newSecondsExtra) public onlyOwner {
+        secondsExtra = newSecondsExtra;
+    }
+
+    function setInitialSecondsUntilWithdrawal(uint256 newInitialSecondsUntilWithdrawal) public onlyOwner {
+        initialSecondsUntilWithdrawal = newInitialSecondsUntilWithdrawal;
+    }
+
+    function updateWithdrawalFraction(uint256 newWithdrawalFraction) public onlyOwner {
+        withdrawalFraction = newWithdrawalFraction;
+    }
+
+    function updateInitalBidAmountFraction(uint256 newInitalBidAmountFraction) public onlyOwner {
+        initalBidAmountFraction = newInitalBidAmountFraction;
+    }
+
+}
