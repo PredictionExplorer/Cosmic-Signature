2163dacb1beec49bd892ce3c2f6887ff4da6e7bd
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2023-02-19 15:53:28 -0600

    Implement the token contract

---
+++ contracts/token.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: CC0-1.0
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+pragma solidity ^0.8.18;
+
+contract ERC20_Token_Sample is ERC20, ERC20Burnable {
+
+    address public biddingWarContract;
+
+    constructor(address _biddingWarContract) ERC20("Obital Token", "ORB") {
+        biddingWarContract = _biddingWarContract;
+    }
+
+    function mint(address owner, uint256 amount) public {
+        require (_msgSender() == biddingWarContract);
+        _mint(owner, amount);
+    }
+
+}
