294d002aade21b056f39770f34ea5a0b0dc1f99a
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2023-02-20 18:30:20 -0600

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,43 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require('hardhat-abi-exporter');
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20000,
+      },
+    },
+  },
+  abiExporter: {
+    path: './abi',
+    clear: true,
+    flat: true,
+    only: ['OrbitalToken', 'Orbitals', 'BiddingWar'],
+    spacing: 2,
+    pretty: false,
+  },
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkarby: {
+      url: `https://rinkeby.arbitrum.io/rpc`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    arbitrum: {
+      url: `https://arb1.arbitrum.io/rpc`,
+      accounts: process.env.MAINNET_PRIVATE_KEY !== undefined ? [process.env.MAINNET_PRIVATE_KEY] : [],
+    },
+
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  }
 };
