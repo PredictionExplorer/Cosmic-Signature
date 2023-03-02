cc53cc7ceaae7a6e0126a1c39bd48743f3c159e7
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2023-03-01 23:08:28 -0600

    Improve the simulation

--- simulation.py
+++ simulation.py
@@ -1,11 +1,14 @@
 class Simulation:
 
     def __init__(self):
-        self.amount = 10
-        self.bid = self.amount / 2000
+        self.balance = 10
+        self.initialBidAmountFraction = 1000
+        self.bid = self.balance / self.initialBidAmountFraction
         self.price_increase = 1.01
         self.time_extra = 1
         self.time_increase = 1.0001
+        self.charityPercentage = 0.10
+        self.withdrawalPercentage = 0.50
 
     def simulate_time(self):
         num_bids = 0
@@ -22,17 +25,21 @@ class Simulation:
                 print_target += 365
 
     def simulate_bids(self, num_bids):
+        num_withdrawals = 0
         for num_bids in range(num_bids):
-            self.amount += self.bid
+            self.balance += self.bid
             self.bid *= self.price_increase
-            prize = self.amount / 2
+            prize = self.balance * self.withdrawalPercentage
+            charity = self.balance * self.charityPercentage
             ratio = self.bid / prize
             days = num_bids // 24
 
-            print(f"days {days} num bids {num_bids:} bid size {self.bid:.2f} prize: {prize:.2f} ratio: {ratio:.2f}")
-            if self.bid > 1:
-                self.amount -= prize
-                self.bid = self.amount / 2000
+            if self.bid > 2:
+                num_withdrawals += 1
+                print(f"days: {days} num bids: {num_bids:} num withdrawals: {num_withdrawals} bid size: {self.bid:.2f} prize: {prize:.2f} ratio: {ratio:.2f}")
+                self.balance -= prize
+                self.balance -= charity
+                self.bid = self.balance / (self.initialBidAmountFraction * 2)
 
 s = Simulation()
-s.simulate_bids(2000)
+s.simulate_bids(10_000)
