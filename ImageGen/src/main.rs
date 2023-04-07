bddd29ca9cd804b82022c957cc37732409e5deed
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2023-04-07 01:29:09 -0500

    copy Python implementation

--- ImageGen/src/main.rs
+++ ImageGen/src/main.rs
@@ -78,7 +78,20 @@ fn update_accelerations(bodies: &mut [Body]) {
     }
 }
 
-fn update_velocities(bodies: &mut [Body], dt: f64) {
+fn gravity_acceleration(body: &Body, dir: Vector3<f64>) -> Vector3<f64> {
+    /*Calculates gravitational acceleration applying on a body.
+    :param m: Mass of object that creates acceleration
+    :param r: Distance between objects
+    */
+    //const G: f64 = 9.8;
+    let mag = dir.norm();
+    return -G * body.mass * dir / (mag * mag * mag);
+}
+
+
+
+/*
+fn update_velocities(body: Body, direction: Vector3) {
     for body in bodies.iter_mut() {
         body.velocity += body.acceleration * dt;
     }
@@ -91,6 +104,51 @@ fn verlet_step(bodies: &mut [Body], dt: f64) {
     }
     update_velocities(bodies, dt);
 }
+*/
+
+
+
+
+fn verlet_step_2(bodies: &mut [Body], dt: f64) {
+    /*Calculates velocity and position of three bodies for the next time step.
+    :param r: Positions of three bodies - #bodies x #dimensions
+    :param v: Velocities of three bodies - #bodies x #dimensions
+    :param m: Masses of three bodies - #bodies x 1
+    :param dt: Time-step in seconds
+    */
+
+
+    let mut a = [Vector3::zeros(), Vector3::zeros(), Vector3::zeros()];
+
+    //a = np.zeros_like(r, dtype=np.float64) #initialize acceleration for all bodies: #bodies x #dimensions
+    //
+    for i in 0..bodies.len() {
+        for j in 0..bodies.len() {
+            if i != j {
+                a[i] += gravity_acceleration(&bodies[j], bodies[i].position - bodies[j].position);
+            }
+        }
+    }
+
+    for i in 0..bodies.len() {
+        bodies[i].position = bodies[i].position + bodies[i].velocity * dt + 0.5 * a[i] * (dt * dt);
+    }
+
+    let a_new = [Vector3::zeros(), Vector3::zeros(), Vector3::zeros()];
+    for i in 0..bodies.len() {
+        for j in 0..bodies.len() {
+            if i != j {
+                a[i] += gravity_acceleration(&bodies[j], bodies[i].position - bodies[j].position);
+            }
+        }
+    }
+
+    for i in 0..bodies.len() {
+        bodies[i].velocity = bodies[i].velocity + 0.5 * (a[i] + a_new[i]) * dt;
+    }
+}
+
+
 
 
 use image::{ImageBuffer, Rgb};
@@ -111,9 +169,9 @@ fn plot_positions(positions: &Vec<Vec<Vector3<f64>>>) {
     let (mut min_x, mut min_y) = (INFINITY, INFINITY);
     let (mut max_x, mut max_y) = (NEG_INFINITY, NEG_INFINITY);
 
-    for i in 0..positions[0].len() - 1 {
-        let x = positions[0][i][0];
-        let y = positions[0][i][1];
+    for i in 0..positions[1].len() - 1 {
+        let x = positions[1][i][0];
+        let y = positions[1][i][1];
         if x < min_x { min_x = x; }
         if y < min_y { min_y = y; }
         if x > max_x { max_x = x; }
@@ -130,11 +188,11 @@ fn plot_positions(positions: &Vec<Vec<Vector3<f64>>>) {
     let mut img = ImageBuffer::from_fn(width, height, |_, _| background_color);
 
     // Draw lines between consecutive positions
-    for i in 0..positions[0].len() - 1 {
-        let x1 = positions[0][i][0];
-        let y1 = positions[0][i][1];
-        let x2 = positions[0][i + 1][0];
-        let y2 = positions[0][i + 1][1];
+    for i in 0..positions[1].len() - 1 {
+        let x1 = positions[1][i][0];
+        let y1 = positions[1][i][1];
+        let x2 = positions[1][i + 1][0];
+        let y2 = positions[1][i + 1][1];
 
         // Scale and shift positions to fit within the image dimensions
         let x1p = ((x1 - min_x) / (max_x - min_x) * (width as f64 - 1.0)).round();
@@ -158,43 +216,21 @@ fn TypeOf<T>(_: &T) {
 
 fn main() {
     let dt = 0.001;
-    let steps = 200_000;
+    let steps = 800_000;
 
     /*
     let body1 = Body::new(42.87781762, Vector3::new(133.47426179, -95.95830491, 16.90552867), Vector3::new(0.0, 0.0, 0.0));
     let body2 = Body::new(18.06659753, Vector3::new(19.90033799, 247.02239617, 70.52390568), Vector3::new(0.0, 0.0, 0.0));
     let body3 = Body::new(105.38238772, Vector3::new(-36.24854541, -25.77961209, -109.48262213), Vector3::new(0.0, 0.0, 0.0));
-    */
 
     let body1 = Body::new(10.0, Vector3::new(-10.0, 10.0, -11.0), Vector3::new(-3.0, 0.0, 0.0));
     let body2 = Body::new(20.0, Vector3::new(0.0, 0.0, 0.0), Vector3::new(0.0, 0.0, 0.0));
     let body3 = Body::new(30.0, Vector3::new(10.0, 10.0, 12.0), Vector3::new(3.0, 0.0, 0.0));
+    */
 
-
-
-
-
-    /*
-m_1 = 10
-m_2 = 20
-m_3 = 30
-
-# starting coordinates for planets
-# p1_start = x_1, y_1, z_1
-p1_start = np.array([-10, 10, -11])
-v1_start = np.array([-3, 0, 0])
-
-# p2_start = x_2, y_2, z_2
-p2_start = np.array([0, 0, 0])
-v2_start = np.array([0, 0, 0])
-
-# p3_start = x_3, y_3, z_3
-p3_start = np.array([10, 10, 12])
-v3_start = np.array([3, 0, 0])
-
-*/
-
-
+    let body1 = Body::new(71.75203285, Vector3::new(138.56428574, -235.17280379, -169.68820646), Vector3::new(0.0, 0.0, 0.0));
+    let body2 = Body::new(24.72652452, Vector3::new(139.56263714, -134.93432058,  -89.39675993), Vector3::new(0.0, 0.0, 0.0));
+    let body3 = Body::new(83.12462743, Vector3::new(-40.34692494, -120.48855271, -107.46054229), Vector3::new(0.0, 0.0, 0.0));
 
 
     let mut bodies = vec![body1, body2, body3];
@@ -205,7 +241,7 @@ v3_start = np.array([3, 0, 0])
         for (i, body) in bodies.iter().enumerate() {
             positions[i][step] = body.position;
         }
-        verlet_step(&mut bodies, dt);
+        verlet_step_2(&mut bodies, dt);
     }
     //println!("{}", positions[0]);
 
