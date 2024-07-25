c6597b22c694530ad1193c165abed9fc573000f3
Me <afterether@gmail.com>
2024-07-24 20:08:26 -0600

    Fixes for all unit tests after all changes in Solidity

---
+++ test/Donating.js
@@ -0,0 +1,66 @@
+const { time, loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { ethers } = require("hardhat");
+const { chai } = require("@nomicfoundation/hardhat-chai-matchers");
+const { expect } = require("chai");
+const SKIP_LONG_TESTS = "1";
+const { basicDeployment,basicDeploymentAdvanced } = require("../src//Deploy.js");
+
+describe("Cosmic Set1", function () {
+	// We define a fixture to reuse the same setup in every test.
+	// We use loadFixture to run this setup once, snapshot that state,
+	// and reset Hardhat Network to that snapshot in every test.
+	async function deployCosmic(deployerAcct) {
+		let contractDeployerAcct;
+		[contractDeployerAcct] = await ethers.getSigners();
+		const {
+			cosmicGame,
+			cosmicToken,
+			cosmicSignature,
+			charityWallet,
+			cosmicDAO,
+			raffleWallet,
+			randomWalkNFT,
+			stakingWallet,
+			marketingWallet,
+		} = await basicDeployment(contractDeployerAcct, "", 0, "0x70997970C51812dc3A010C7d01b50e0d17dc79C8", true,true);
+
+		return {
+			cosmicGame,
+			cosmicToken,
+			cosmicSignature,
+			charityWallet,
+			cosmicDAO,
+			randomWalkNFT,
+			raffleWallet,
+			stakingWallet,
+			marketingWallet,
+		};
+	}
+	const bidParamsEncoding = {
+		type: "tuple(string,int256)",
+		name: "bidparams",
+		components: [
+			{ name: "msg", type: "string" },
+			{ name: "rwalk", type: "int256" },
+		],
+	};
+	it("donateWithInfo() works as expected", async function () {
+		[owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+		const { cosmicGame, cosmicToken, cosmicSignature, charityWallet, cosmicDAO, raffleWallet, randomWalkNFT } =
+			await loadFixture(deployCosmic);
+		let = contractErrors = await ethers.getContractFactory("CosmicGameErrors");
+		let donationAmount = ethers.utils.parseEther("10");
+		let dataStr ="{'version':1,'url':'http://one.two/three'}";
+		await cosmicGame.connect(addr1).donateWithInfo(dataStr,{ value: donationAmount });
+		let numDonationInfoRecs = await cosmicGame.donateWithInfoNumRecords();
+		expect(numDonationInfoRecs).to.equal(1);
+		let donationInfoRec = await cosmicGame.getDonationInfoRecord(0);
+		expect(donationInfoRec.donor).to.equal(addr1.address);
+		expect(donationInfoRec.data).to.equal(dataStr);
+
+		// check number of records is incrementing
+		await cosmicGame.connect(addr1).donateWithInfo(dataStr,{ value: donationAmount });
+		numDonationInfoRecs = await cosmicGame.donateWithInfoNumRecords();
+		expect(numDonationInfoRecs).to.equal(2);
+	});
+});
