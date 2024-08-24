088835b86b8b6043c14852c85ca65f5936dd5847
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2024-08-23 21:19:06 -0400

    Added ICosmicSignature.

--- contracts/production/CosmicSignature.sol
+++ contracts/production/CosmicSignature.sol
@@ -2,17 +2,16 @@
 
 pragma solidity 0.8.26;
 
-import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import { ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { CosmicGameErrors } from "./libraries/CosmicGameErrors.sol";
 import { MyERC721Enumerable } from "./MyERC721Enumerable.sol";
+import { ICosmicSignature } from "./interfaces/ICosmicSignature.sol";
 
-/// @title CosmicSignature - NFT for the Cosmic Game ecosystem
-/// @author Cosmic Game Development Team
-/// @notice This contract implements the CosmicSignature NFT with unique features for the Cosmic Game
 /// @dev Extends MyERC721Enumerable and includes custom minting and metadata management
-contract CosmicSignature is MyERC721Enumerable, Ownable {
+contract CosmicSignature is MyERC721Enumerable, Ownable, ICosmicSignature {
+	// #region State
+
 	/// @notice Mapping of token IDs to their unique seeds
 	mapping(uint256 => bytes32) public seeds;
 
@@ -34,27 +33,10 @@ contract CosmicSignature is MyERC721Enumerable, Ownable {
 	/// @notice IPFS link to the script that generates images and videos for each NFT based on seed
 	string public tokenGenerationScriptURL = "ipfs://TBD";
 
-	/// @notice Emitted when a token's name is set or changed
-	/// @param tokenId The ID of the token
-	/// @param newName The new name of the token
-	event TokenNameEvent(uint256 indexed tokenId, string newName);
-
-	/// @notice Emitted when a new token is minted
-	/// @param tokenId The ID of the newly minted token
-	/// @param owner The address that received the token
-	/// @param roundNum The round number in which the token was minted
-	/// @param seed The unique seed generated for the token
-	event MintEvent(uint256 indexed tokenId, address indexed owner, uint256 indexed roundNum, bytes32 seed);
-
-	/// @notice Emitted when the token generation script URL is updated
-	/// @param newURL The new URL for the token generation script
-	event TokenGenerationScriptURLEvent(string newURL);
-
-	/// @notice Emitted when the base URI is updated
-	/// @param newURI The new base URI
-	event BaseURIEvent(string newURI);
+	// #endregion
 
 	/// @notice Initializes the CosmicSignature contract
+	/// todo-1 Why do we need the above comment? Don't we know what a constructor does?
 	/// @param _cosmicGameProxyContract The address of the CosmicGameProxy contract.
 	/// ToDo-202408114-1 applies.
 	constructor(address _cosmicGameProxyContract) ERC721("CosmicSignature", "CSS") Ownable(msg.sender) {
@@ -63,24 +45,17 @@ contract CosmicSignature is MyERC721Enumerable, Ownable {
 		cosmicGameProxyContract = _cosmicGameProxyContract;
 	}
 
-	/// @notice Sets the URL for the token generation script
-	/// @param newTokenGenerationScriptURL The new URL to set
-	function setTokenGenerationScriptURL(string memory newTokenGenerationScriptURL) external onlyOwner {
+	function setTokenGenerationScriptURL(string memory newTokenGenerationScriptURL) external override onlyOwner {
 		tokenGenerationScriptURL = newTokenGenerationScriptURL;
 		emit TokenGenerationScriptURLEvent(newTokenGenerationScriptURL);
 	}
 
-	/// @notice Sets the base URI for token metadata
-	/// @param baseURI The new base URI to set
-	function setBaseURI(string memory baseURI) external onlyOwner {
+	function setBaseURI(string memory baseURI) external override onlyOwner {
 		_baseTokenURI = baseURI;
 		emit BaseURIEvent(baseURI);
 	}
 
-	/// @notice Allows token owners to set a custom name for their token
-	/// @param tokenId The ID of the token to name
-	/// @param name The custom name to set for the token
-	function setTokenName(uint256 tokenId, string memory name) external {
+	function setTokenName(uint256 tokenId, string memory name) external override {
 		require(
 			_isApprovedOrOwner(_msgSender(), tokenId),
 			CosmicGameErrors.OwnershipError("setTokenName caller is not owner nor approved.", tokenId)
@@ -93,12 +68,7 @@ contract CosmicSignature is MyERC721Enumerable, Ownable {
 		emit TokenNameEvent(tokenId, name);
 	}
 
-	/// @notice Mints a new CosmicSignature token
-	/// @dev Only callable by the CosmicGameProxy contract.
-	/// @param owner The address that will receive the newly minted token
-	/// @param roundNum The round number in which the token is minted
-	/// @return The ID of the newly minted token
-	function mint(address owner, uint256 roundNum) external returns (uint256) {
+	function mint(address owner, uint256 roundNum) external override returns (uint256) {
 		require(owner != address(0), CosmicGameErrors.ZeroAddress("Zero-address was given."));
 		require(
 			_msgSender() == cosmicGameProxyContract,
@@ -117,9 +87,9 @@ contract CosmicSignature is MyERC721Enumerable, Ownable {
 		return tokenId;
 	}
 
-	/// @notice Returns the base URI for token metadata
-	/// @return The base URI string
-	function _baseURI() internal view virtual override returns (string memory) {
-		return _baseTokenURI;
-	}
+	// /// @return The base URI for token metadata
+	// /// @dev ToDo-202408241-1 applies.
+	// function _baseURI() internal view virtual override returns (string memory) {
+	// 	return _baseTokenURI;
+	// }
 }
