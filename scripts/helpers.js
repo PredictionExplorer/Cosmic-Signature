58b959e0dae8c370522d453bc37e73f7cf9407e9
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-02-16 13:10:39 -0500

    Improved contract deployment helpers and tasks.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -4,24 +4,41 @@
 
 const hre = require("hardhat");
 
+// todo-1 This should return `{cosmicSignatureGameAddr, cosmicSignatureGame}`.
 async function getCosmicSignatureGameContract(cosmicSignatureGameContractName = "CosmicSignatureGame") {
-	let cosmicSignatureGameAddr = process.env.COSMIC_SIGNATURE_GAME_ADDRESS;
-	if (typeof cosmicSignatureGameAddr === "undefined" || cosmicSignatureGameAddr.length != 42) {
+	const cosmicSignatureGameAddr = process.env.COSMIC_SIGNATURE_GAME_ADDRESS;
+	// todo-1 Unnecessary to validate length?
+	if (typeof cosmicSignatureGameAddr === "undefined" || cosmicSignatureGameAddr.length !== 42) {
 		console.log("COSMIC_SIGNATURE_GAME_ADDRESS environment variable does not contain contract address.");
 		process.exit(1);
 	}
-	let cosmicSignatureGame = await hre.ethers.getContractAt(cosmicSignatureGameContractName, cosmicSignatureGameAddr);
+
+	// [Comment-202502096]
+	// Issue. This uses default signer. Is it a problem?
+	// todo-1 Everywhere we call `getContractAt`, think about the above.
+	// todo-1 Everywhere we call `getContractAt`, consider calling `attach`. See `deployContractsAdvanced`.
+	// todo-1 Also `getContractFactory`.
+	// todo-1 Mybe add a param: `ownerAcct`, similar to `deployerAcct` in some other functions.
+	// todo-1 Some scripts use the `PRIVKEY` environment variable. So the caller can pass that wallet to us.
+	// [/Comment-202502096]
+	const cosmicSignatureGame = await hre.ethers.getContractAt(cosmicSignatureGameContractName, cosmicSignatureGameAddr);
+
 	return cosmicSignatureGame;
 }
 
+// todo-1 This should return `{bidderContractAddr, bidderContract}`.
 async function getBidderContract() {
-	let bidderContractAddr = process.env.BIDDER_CONTRACT_ADDRESS;
-	if (typeof bidderContractAddr === "undefined" || bidderContractAddr.length != 42) {
+	const bidderContractAddr = process.env.BIDDER_CONTRACT_ADDRESS;
+	// todo-1 Unnecessary to validate length?
+	if (typeof bidderContractAddr === "undefined" || bidderContractAddr.length !== 42) {
 		console.log("BIDDER_CONTRACT_ADDRESS environment variable does not contain contract address.");
 		process.exit(1);
 	}
 	// console.log(bidderContractAddr);
-	let bidderContract = await hre.ethers.getContractAt("BidderContract", bidderContractAddr);
+
+	// Comment-202502096 applies.
+	const bidderContract = await hre.ethers.getContractAt("BidderContract", bidderContractAddr);
+
 	return bidderContract;
 }
 
---
+++ src/ContractDeploymentHelpers.js
@@ -0,0 +1,205 @@
+// #region
+
+"use strict";
+
+// #endregion
+// #region
+
+// [Comment-202409255]
+// Because "hardhat.config.js" imports us, an attempt to import "hardhat" here would throw an error.
+// So we must do things differently here.
+// [/Comment-202409255]
+// const hre = require("hardhat");
+const { HardhatContext } = require("hardhat/internal/context");
+
+// #endregion
+// #region `deployContracts`
+
+/**
+ * @param {import("ethers").BaseWallet} deployerAcct 
+ * @param {string} randomWalkNftAddr 
+ * @param {string} charityAddr 
+ * @param {boolean} transferOwnershipToCosmicSignatureDao 
+ * @param {number} roundActivationTime 
+ * @returns 
+ */
+const deployContracts = async function (
+	deployerAcct,
+	randomWalkNftAddr,
+	charityAddr,
+	transferOwnershipToCosmicSignatureDao,
+	roundActivationTime
+) {
+	return await deployContractsAdvanced(
+		deployerAcct,
+		"CosmicSignatureGame",
+		randomWalkNftAddr,
+		charityAddr,
+		transferOwnershipToCosmicSignatureDao,
+		roundActivationTime
+	);
+};
+
+// #endregion
+// #region `deployContractsAdvanced`
+
+/**
+ * @param {import("ethers").BaseWallet} deployerAcct 
+ * @param {string} cosmicSignatureGameContractName 
+ * @param {string} randomWalkNftAddr May be empty.
+ * @param {string} charityAddr 
+ * @param {boolean} transferOwnershipToCosmicSignatureDao 
+ * @param {number} roundActivationTime 
+ * Possible values:
+ *    0: leave the default value hardcoded in the contract.
+ *    1: use the latest block timestamp.
+ *    Any other value: use the given value as is.
+ * @returns 
+ */
+const deployContractsAdvanced = async function (
+	deployerAcct,
+	cosmicSignatureGameContractName,
+	randomWalkNftAddr,
+	charityAddr,
+	transferOwnershipToCosmicSignatureDao,
+	roundActivationTime
+) {
+	// Comment-202409255 applies.
+	const hre = HardhatContext.getHardhatContext().environment;
+
+	const cosmicSignatureGameFactory = await hre.ethers.getContractFactory(cosmicSignatureGameContractName, deployerAcct);
+
+	// Comment-202412129 relates.
+	const cosmicSignatureGameProxy =
+		await hre.upgrades.deployProxy(
+			cosmicSignatureGameFactory,
+			[deployerAcct.address],
+			{
+				kind: "uups"
+			}
+		);
+		
+	await cosmicSignatureGameProxy.waitForDeployment();
+	const cosmicSignatureGameProxyAddr = await cosmicSignatureGameProxy.getAddress();
+
+	const cosmicSignatureGameImplementationAddr = await hre.upgrades.erc1967.getImplementationAddress(cosmicSignatureGameProxyAddr);
+	const cosmicSignatureGameImplementation = cosmicSignatureGameFactory.attach(cosmicSignatureGameImplementationAddr);
+
+	const cosmicSignatureNftFactory = await hre.ethers.getContractFactory("CosmicSignatureNft", deployerAcct);
+	const cosmicSignatureNft = await cosmicSignatureNftFactory.deploy(cosmicSignatureGameProxyAddr);
+	await cosmicSignatureNft.waitForDeployment();
+	const cosmicSignatureNftAddr = await cosmicSignatureNft.getAddress();
+
+	const cosmicSignatureTokenFactory = await hre.ethers.getContractFactory("CosmicSignatureToken", deployerAcct);
+	const cosmicSignatureToken = await cosmicSignatureTokenFactory.deploy(cosmicSignatureGameProxyAddr);
+	await cosmicSignatureToken.waitForDeployment();
+	const cosmicSignatureTokenAddr = await cosmicSignatureToken.getAddress();
+	// await cosmicSignatureToken.transferOwnership(cosmicSignatureGameProxyAddr);
+
+	const cosmicSignatureDaoFactory = await hre.ethers.getContractFactory("CosmicSignatureDao", deployerAcct);
+	const cosmicSignatureDao = await cosmicSignatureDaoFactory.deploy(cosmicSignatureTokenAddr);
+	await cosmicSignatureDao.waitForDeployment();
+	const cosmicSignatureDaoAddr = await cosmicSignatureDao.getAddress();
+
+	const charityWalletFactory = await hre.ethers.getContractFactory("CharityWallet", deployerAcct);
+	const charityWallet = await charityWalletFactory.deploy();
+	await charityWallet.waitForDeployment();
+	const charityWalletAddr = await charityWallet.getAddress();
+	await charityWallet.setCharityAddress(charityAddr);
+	if (transferOwnershipToCosmicSignatureDao) {
+		// It appears that it makes no sense to perform this kind of ownership transfer for any other contracts.
+		await charityWallet.transferOwnership(cosmicSignatureDaoAddr);
+	}
+
+	const prizesWalletFactory = await hre.ethers.getContractFactory("PrizesWallet", deployerAcct);
+	const prizesWallet = await prizesWalletFactory.deploy(cosmicSignatureGameProxyAddr);
+	await prizesWallet.waitForDeployment();
+	const prizesWalletAddr = await prizesWallet.getAddress();
+
+	const randomWalkNftFactory = await hre.ethers.getContractFactory("RandomWalkNFT", deployerAcct);
+	let randomWalkNft;
+	if (randomWalkNftAddr.length === 0) {
+		randomWalkNft = await randomWalkNftFactory.deploy();
+		await randomWalkNft.waitForDeployment();
+		randomWalkNftAddr = await randomWalkNft.getAddress();
+	} else {
+		randomWalkNft = randomWalkNftFactory.attach(randomWalkNftAddr);
+	}
+
+	const stakingWalletCosmicSignatureNftFactory = await hre.ethers.getContractFactory("StakingWalletCosmicSignatureNft", deployerAcct);
+	const stakingWalletCosmicSignatureNft =
+		await stakingWalletCosmicSignatureNftFactory.deploy(
+			cosmicSignatureNftAddr,
+			cosmicSignatureGameProxyAddr
+		);
+	await stakingWalletCosmicSignatureNft.waitForDeployment();
+	const stakingWalletCosmicSignatureNftAddr = await stakingWalletCosmicSignatureNft.getAddress();
+
+	const stakingWalletRandomWalkNftFactory = await hre.ethers.getContractFactory("StakingWalletRandomWalkNft", deployerAcct);
+	const stakingWalletRandomWalkNft = await stakingWalletRandomWalkNftFactory.deploy(randomWalkNftAddr);
+	await stakingWalletRandomWalkNft.waitForDeployment();
+	const stakingWalletRandomWalkNftAddr = await stakingWalletRandomWalkNft.getAddress();
+
+	const marketingWalletFactory = await hre.ethers.getContractFactory("MarketingWallet", deployerAcct);
+	const marketingWallet = await marketingWalletFactory.deploy(cosmicSignatureTokenAddr);
+	await marketingWallet.waitForDeployment();
+	const marketingWalletAddr = await marketingWallet.getAddress();
+
+	await cosmicSignatureGameProxy.setCosmicSignatureToken(cosmicSignatureTokenAddr);
+	await cosmicSignatureGameProxy.setRandomWalkNft(randomWalkNftAddr);
+	await cosmicSignatureGameProxy.setCosmicSignatureNft(cosmicSignatureNftAddr);
+	await cosmicSignatureGameProxy.setPrizesWallet(prizesWalletAddr);
+	await cosmicSignatureGameProxy.setStakingWalletRandomWalkNft(stakingWalletRandomWalkNftAddr);
+	await cosmicSignatureGameProxy.setStakingWalletCosmicSignatureNft(stakingWalletCosmicSignatureNftAddr);
+	await cosmicSignatureGameProxy.setMarketingWallet(marketingWalletAddr);
+	await cosmicSignatureGameProxy.setCharityAddress(charityWalletAddr);
+	if (roundActivationTime != 0) {
+		if (roundActivationTime == 1) {
+			const latestBlock = await hre.ethers.provider.getBlock("latest");
+			roundActivationTime = latestBlock.timestamp + 1;
+		}
+		await cosmicSignatureGameProxy.setRoundActivationTime(roundActivationTime);
+	}
+
+	return {
+		cosmicSignatureGameFactory,
+		cosmicSignatureGameImplementation,
+		cosmicSignatureGameImplementationAddr,
+		cosmicSignatureGameProxy,
+		cosmicSignatureGameProxyAddr,
+		cosmicSignatureNftFactory,
+		cosmicSignatureNft,
+		cosmicSignatureNftAddr,
+		cosmicSignatureTokenFactory,
+		cosmicSignatureToken,
+		cosmicSignatureTokenAddr,
+		cosmicSignatureDaoFactory,
+		cosmicSignatureDao,
+		cosmicSignatureDaoAddr,
+		charityWalletFactory,
+		charityWallet,
+		charityWalletAddr,
+		prizesWalletFactory,
+		prizesWallet,
+		prizesWalletAddr,
+		randomWalkNftFactory,
+		randomWalkNft,
+		randomWalkNftAddr,
+		stakingWalletCosmicSignatureNftFactory,
+		stakingWalletCosmicSignatureNft,
+		stakingWalletCosmicSignatureNftAddr,
+		stakingWalletRandomWalkNftFactory,
+		stakingWalletRandomWalkNft,
+		stakingWalletRandomWalkNftAddr,
+		marketingWalletFactory,
+		marketingWallet,
+		marketingWalletAddr,
+	};
+};
+
+// #endregion
+// #region
+
+module.exports = { deployContracts, deployContractsAdvanced };
+
+// #endregion
