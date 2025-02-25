bea6ff34b3a48a280904b7e2398026c5c5f1c799
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-02-25 09:08:38 -0500

    Minor refactorings.

--- contracts/production/CosmicSignatureDao.sol
+++ contracts/production/CosmicSignatureDao.sol
@@ -1,5 +1,10 @@
+// #region
+
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity 0.8.28;
+
+// #endregion
+// #region
 
 import { IERC6372 } from "@openzeppelin/contracts/interfaces/IERC6372.sol";
 import { IVotes } from "@openzeppelin/contracts/governance/utils/IVotes.sol";
@@ -9,23 +14,35 @@ import { GovernorCountingSimple } from "@openzeppelin/contracts/governance/exten
 import { GovernorVotes } from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import { GovernorVotesQuorumFraction } from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import { CosmicSignatureConstants } from "./libraries/CosmicSignatureConstants.sol";
+import { AddressValidator } from "./AddressValidator.sol";
 import { ICosmicSignatureDao } from "./interfaces/ICosmicSignatureDao.sol";
 
+// #endregion
+// #region
+
+/// @dev todo-1 +++ Compare this code to what we had before I joined the project. Done on Feb 24 2025.
 contract CosmicSignatureDao is
 	Governor,
 	GovernorSettings,
 	GovernorCountingSimple,
 	GovernorVotes,
 	GovernorVotesQuorumFraction,
+	AddressValidator,
 	ICosmicSignatureDao {
+	// #region `constructor`
+
 	/// @notice Constructor.
 	/// Sets up the governance parameters and links the voting token.
 	/// @param tokenAddress_ The address of the token to be used for voting power.
+	/// We use the `CosmicSignatureToken` contract address for this parameter.
 	constructor(IVotes tokenAddress_)
+		// Comment-202502249 relates and/or applies.
+		_providedAddressIsNonZero(address(tokenAddress_))
+
 		Governor("CosmicSignatureDao")
 
 		// [Comment-202501123]
-		// By default, `Governor`'s `votingDelay` and `votingPeriod` are expessed in the number of blocks.
+		// By default, `Governor`'s `votingDelay()` and `votingPeriod()` are expessed in the number of blocks.
 		// I changed those to be expressed in seconds.
 		// OpenZellepin docs says:
 		//    The internal clock used by the token to store voting balances will dictate the operating mode
@@ -39,35 +56,46 @@ contract CosmicSignatureDao is
 			CosmicSignatureConstants.DEFAULT_CST_REWARD_AMOUNT_FOR_BIDDING
 		)
 
+		// [Comment-202502249]
+		// Issue. Surprisingly, this doesn't validate that the provided address is a nonzero.
+		// So we have to do it.
+		// [/Comment-202502249]
 		GovernorVotes(tokenAddress_)
+
 		// todo-1 There are setters for these settings. Do I need to develop tests that change them?
 		GovernorVotesQuorumFraction(CosmicSignatureConstants.GOVERNOR_DEFAULT_VOTES_QUORUM_PERCENTAGE) {
 	}
 
-	// The following functions are overrides required by Solidity.
+	// #endregion
+	// #region Overrides Required By Solidity
 
-	/// todo-1 Develop a test that tests what these return: proposalThreshold, votingDelay, votingPeriod, quorum. Done in part.
-	function proposalThreshold() public view override(IGovernor, Governor, GovernorSettings) returns (uint256) {
-		return super.proposalThreshold();
-	}
+	/// todo-1 Develop a test that tests that these methods return the correct values. Done in part.
 
-	function votingDelay() public view override(IGovernor, Governor, GovernorSettings) returns (uint256) {
+	function votingDelay() public view override (IGovernor, Governor, GovernorSettings) returns (uint256) {
 		return super.votingDelay();
 	}
 
-	function votingPeriod() public view override(IGovernor, Governor, GovernorSettings) returns (uint256) {
+	function votingPeriod() public view override (IGovernor, Governor, GovernorSettings) returns (uint256) {
 		return super.votingPeriod();
 	}
 
-	function quorum(uint256 timepoint_) public view override(IGovernor, Governor, GovernorVotesQuorumFraction) returns (uint256) {
+	function proposalThreshold() public view override (IGovernor, Governor, GovernorSettings) returns (uint256) {
+		return super.proposalThreshold();
+	}
+
+	function quorum(uint256 timepoint_) public view override (IGovernor, Governor, GovernorVotesQuorumFraction) returns (uint256) {
 		return super.quorum(timepoint_);
 	}
 
-	function CLOCK_MODE() public view override(IERC6372, Governor, GovernorVotes) returns (string memory) {
+	function CLOCK_MODE() public view override (IERC6372, Governor, GovernorVotes) returns (string memory) {
 		return super.CLOCK_MODE();
 	}
 
-	function clock() public view override(IERC6372, Governor, GovernorVotes) returns (uint48) {
+	function clock() public view override (IERC6372, Governor, GovernorVotes) returns (uint48) {
 		return super.clock();
 	}
+
+	// #endregion
 }
+
+// #endregion
