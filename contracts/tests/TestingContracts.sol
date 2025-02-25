ce3fa9cbb8cbc568970b6e931e2c0afa09b5df2b
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-02-25 09:07:54 -0500

    Added a TODO.

--- contracts/tests/TestingContracts.sol
+++ contracts/tests/TestingContracts.sol
@@ -285,6 +285,7 @@ contract SpecialCosmicSignatureGame is CosmicSignatureGame {
 // 	// }
 // }
 
+/// @dev todo-1 Do I need to refactor this similarly to `MaliciousNft2`?
 contract MaliciousNft1 is ERC721 {
 	constructor(string memory name_, string memory symbol_) ERC721(name_,symbol_) {
 	}
