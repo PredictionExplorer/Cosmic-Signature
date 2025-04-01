5233637489b2bcd2e8aed52f9ea6bf5300395314
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-04-01 15:06:58 -0400

    Added a Bash script.

---
+++ smtchecker/set-process-ram-max-limit.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# Execute this script for the solc process.
+# Pass its process ID as an argument.
+# But before you execute this script, make sure the value of `RamMaxLimit` is OK.
+
+(
+   OutcomeCodeBitMask=0
+   RamMaxLimit="$((64 * 1024 * 1024 * 1024))"
+
+   if ! (( ${OutcomeCodeBitMask} & 2 )); then
+      if [[ $# -eq 1 && -n "${1}" ]]; then
+         :
+      else
+         echo 'Error. Invalid command line.'
+         OutcomeCodeBitMask=$(( OutcomeCodeBitMask | 2 ))
+      fi
+   fi
+
+   if ! (( ${OutcomeCodeBitMask} & 2 )); then
+      'prlimit' '--pid' "${1}" "--as=${RamMaxLimit}"
+      if [[ $? -ne 0 ]]; then
+         echo "Error. prlimit returned $?."
+         OutcomeCodeBitMask=$(( OutcomeCodeBitMask | 2 ))
+      fi
+   fi
+)
