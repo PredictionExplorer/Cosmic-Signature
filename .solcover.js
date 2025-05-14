960ab346940f8ee8a3f1d2e82c1ba3ecd58c7796
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-05-14 14:48:02 -0400

    Added the Solidity Coverage capability to the project.

---
+++ .solcover.js
@@ -0,0 +1,35 @@
+// To test for Solidity Coverage, execute the following command:
+// 
+// 'npx' 'hardhat' 'coverage'
+// 
+// To test only specific 1 or more test files:
+//
+// 'npx' 'hardhat' 'coverage' '--testfiles' 'test/MyTest1.js'
+//  
+// 'npx' 'hardhat' 'coverage' '--testfiles' 'test/{MyTest1.js,MyTest2.js}'
+// 
+// To test only specific tests, specify a reg-exp pattern that matches their titles near Comment-202505171.
+// 
+// Hardhat will create the following folders and files:
+// coverage/
+// coverage.json
+// 
+// "coverage.json" isn't intended to be read by humans.
+//
+// To find parts of the code that are not covered, cd to "coverage/" and search in all files in the folder tree containing:
+//
+// title=
+module.exports = {
+	skipFiles: ["tests/", "upgrade-prototype/",],
+
+	// Issue. Do we really need this?
+	configureYulOptimizer: true,
+
+	istanbulReporter: ["html", /*"text",*/],
+	mocha: {
+		// // [Comment-202505171/]
+		// grep: "multiple bidding rounds$",
+
+		parallel: false,
+	},
+};
