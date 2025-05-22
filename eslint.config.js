0962cb935c356183d575bd2c720a52c4b70b1fed
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-05-22 08:43:24 -0400

    Improved ESLint.

--- eslint.config.js
+++ eslint.config.js
@@ -2,77 +2,35 @@
 
 // [Comment-202506016]
 // Execute the "./eslint/eslint-1.bash" script.
+// To find missing `await` keywords, in the ESLint report file, find:
+// @typescript-eslint/no-floating-promises
 // [/Comment-202506016]
 
-
-// import js from "@eslint/js";
-// import globals from "globals";
-// import { defineConfig } from "eslint/config";
-//
-// export default defineConfig([
-//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-//   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-//   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-// ]);
-
-
 const esLintJs = require("@eslint/js");
-const typeScriptEsLintParser = require("@typescript-eslint/parser");
+const typeScriptEsLint = require("typescript-eslint");
 
-// // Issue. This misses missing `await`s.
-// const esLintPluginNoFloatingPromise = require("eslint-plugin-no-floating-promise");
-
-const typeScriptEsLintEsLintPlugin = require("@typescript-eslint/eslint-plugin");
-
-module.exports = [
-	{
-		// // This will anyway be ignored even if we don't list this here.
-		// ignores: ["node_modules/**"],
-
-		ignores: ["coverage/**"],
-	},
-	{
-		plugins: {
-			// noFloatingPromise: esLintPluginNoFloatingPromise,
-			esLintPlugin: typeScriptEsLintEsLintPlugin,
-		},
-
-		// parser / env settings
-		languageOptions: {
-			parser: typeScriptEsLintParser,
-			parserOptions: {
-				project: "./jsconfig.json",
-
-				// Comment-202505308 applies.
-				ecmaVersion: 2022,
-				// sourceType: "module", // or "script" if you're not using imports
-				sourceType: "script",
-			},
-			globals: {
-				// // browser globals
-				// window: "readonly",
-				// document: "readonly",
-				// navigator: "readonly",
-
-				// node globals
-				process: "readonly",
-				__dirname: "readonly",
-				__filename: "readonly",
-				module: "readonly",
-				require: "readonly",
-				console: "readonly",
+module.exports =
+	typeScriptEsLint.config(
+		esLintJs.configs.recommended,
+		typeScriptEsLint.configs.recommendedTypeChecked,
+		// typeScriptEsLint.configs.strictTypeChecked,
+		{
+			languageOptions: {
+				parserOptions: {
+					projectService: true,
+					tsconfigRootDir: __dirname,
+					// sourceType: "script",
+				},
 			},
 		},
-
-		rules: {
-			// spread in all eslint:recommended rules
-			...esLintJs.configs.recommended.rules,
-	
-			// // Enable the no-floating-promise rule.
-			// "noFloatingPromise/no-floating-promise": "error",
-
-			// Enable the TS "no-floating-promises" rule.
-			"esLintPlugin/no-floating-promises": "error",
-		},
-	},
-];
+		{
+			rules: {
+				"@typescript-eslint/no-require-imports": "off",
+				"@typescript-eslint/no-unsafe-assignment": "off",
+				"@typescript-eslint/no-unsafe-call": "off",
+				"@typescript-eslint/no-unsafe-member-access": "off",
+				"@typescript-eslint/no-unsafe-argument": "off",
+				"@typescript-eslint/no-unsafe-return": "off",
+			},
+		}
+	);
