055c69bffc9d1e8cfc2ddfc0a0132a5eb539a00e
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-06-23 21:13:31 -0400

    Added a helper method.

--- contracts/production/libraries/CosmicSignatureHelpers.sol
+++ contracts/production/libraries/CosmicSignatureHelpers.sol
@@ -12,4 +12,30 @@ library CosmicSignatureHelpers {
 	// }
 
 	// #endregion
+	// #region `transferEthTo`
+
+	/// @dev Issue. In some places where we transfer ETH by calling `address.call`,
+	/// it could make sense to call this method instead.
+	/// But I have no immediate plans to refactorr anything.
+	function transferEthTo(address payable toAddress_, uint256 amount_) internal {
+		// [Comment-202502043]
+		// In most cases, we make high level calls to strongly typed addresses --
+		// to let SMTChecker know what exactly method on what contract we are calling.
+		// But we make a low level call like this to make a simple ETH transfer.
+		// Comment-202502057 relates.
+		// Comment-202506296 relates.
+		// [/Comment-202502043]
+		(bool isSuccess_, ) = toAddress_.call{value: amount_}("");
+
+		if ( ! isSuccess_ ) {
+			assembly {
+				let returnDataSize_ := returndatasize()
+				let freeMemoryPointer_ := mload(0x40)
+				returndatacopy(freeMemoryPointer_, 0, returnDataSize_)
+				revert (freeMemoryPointer_, returnDataSize_)
+			}
+		}
+	}
+
+	// #endregion
 }
