f88a452acd8023346c174f5844c78f221d832038
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-07-17 14:38:57 -0400

    Improved the `CosmicSignatureNft` contract tests. Id=202508028

--- test/test-1.bash
+++ test/test-1.bash
@@ -20,7 +20,7 @@
 			export ENABLE_SMTCHECKER="${3}"
 
 			# todo-0 Uncomment the command line with no arguments.
-			# 'npx' 'hardhat' 'test' '--grep' '^MarketingWallet '
+			# 'npx' 'hardhat' 'test' '--grep' '^CosmicSignatureNft '
 			# 'npx' 'hardhat' 'test' '--grep' '^StakingWalletRandomWalkNft Double-unstaking Random Walk NFTs$'
 			# 'npx' 'hardhat' 'test' '--grep' '(?<!\bLong-term aggressive bidding behaves correctly)$'
 			# 'npx' 'hardhat' 'test' '--grep' '^PrizesWallet-\d |^StakingWalletCosmicSignatureNft |^CharityWallet |^SystemManagement |^BidStatistics |^Bidding |^MainPrize |^CosmicSignatureGame-\d |^BidderContract |^BlockTimeStamps '
