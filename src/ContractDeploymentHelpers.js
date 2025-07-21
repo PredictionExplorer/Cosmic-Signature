3cdbc465170a1125333b56bec1c5439809030613
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-07-21 12:13:44 -0400

    Edited comments. Id=202508077

--- src/ContractDeploymentHelpers.js
+++ src/ContractDeploymentHelpers.js
@@ -196,7 +196,7 @@ const deployContractsAdvanced = async function (
  */
 async function setRoundActivationTimeIfNeeded(cosmicSignatureGameProxy, roundActivationTime) {
 	// [Comment-202507202]
-	// Similar magic numbers are hardcoded in multiple places.
+	// Similar magic numbers exist in multiple places.
 	// [/Comment-202507202]
 	if (roundActivationTime > -1_000_000_000n) {
 
