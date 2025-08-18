27113299cefc7b4a0ce5d835c7be63734e243b7b
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-08-18 14:15:38 -0400

    Refactored "scripts/cosmic-signature-game-eth-donations/donate-eth.js". Id=202509146

--- scripts/cosmic-signature-game-eth-donations/donate-eth.js
+++ scripts/cosmic-signature-game-eth-donations/donate-eth.js
@@ -1,27 +1,10 @@
 "use strict";
 
 const hre = require("hardhat");
-const { getCosmicSignatureGameContract } = require("./helpers.js");
 
-async function main() {
-	let privKey = process.env.PRIVKEY;
-	if (privKey == undefined || privKey.length <= 0) {
-		console.info(
-			// todo-1 "scripts/deploy.js" no longer exists.
-			"Please provide private key on the command line as ENVIRONMENT variable 'PRIVKEY', example : PRIVKEY=\"0x21982349...\" npx hardhat run scripts/deploy.js",
-		);
-		process.exit(1);
-	}
-	let testingAcct = new hre.ethers.Wallet(privKey, hre.ethers.provider);
-	let cosmicSignatureGame = await getCosmicSignatureGameContract();
-
-	const ethDonationAmount = 2n * 10n ** 18n;
-	await cosmicSignatureGame.connect(testingAcct).donateEth({value: ethDonationAmount,});
+async function donateEthToCosmicSignatureGame(cosmicSignatureGameProxy_, donorSigner_, amountInEth_) {
+	const amountInWei_ = hre.ethers.parseEther(amountInEth_.toString());
+	await cosmicSignatureGameProxy_.connect(donorSigner_).donateEth({value: amountInWei_,});
 }
 
-main()
-	.then(() => process.exit(0))
-	.catch(error => {
-		console.error(error);
-		process.exit(1);
-	});
+module.exports { donateEthToCosmicSignatureGame, };
