9f7186f0c00b6a14173c4bac2e009d5c4df3b814
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-08-22 19:25:01 -0400

    Commented out unused code. Id=202509247

--- scripts/selfdestructable-cosmic-signature-game/selfdestruct-test-contract.js
+++ scripts/selfdestructable-cosmic-signature-game/selfdestruct-test-contract.js
@@ -1,26 +1,27 @@
-"use strict";
+// Comment-202509229 applies.
 
-// const { expect } = require("chai");
-// const hre = require("hardhat");
-const { getCosmicSignatureGameContract } = require("./helpers.js");
+// "use strict";
 
-async function main() {
-	// let privKey = process.env.PRIVKEY;
-	// if (privKey == undefined || privKey.length <= 0) {
-	// 	console.info(
-	// 		// todo-1 "scripts/deploy.js" no longer exists.
-	// 		"Please provide private key on the command line as ENVIRONMENT variable 'PRIVKEY', example : PRIVKEY=\"0x21982349...\" npx hardhat run scripts/deploy.js",
-	// 	);
-	// 	process.exit(1);
-	// }
-	// let testingAcct = new hre.ethers.Wallet(privKey, hre.ethers.provider);
-	const cosmicSignatureGame = await getCosmicSignatureGameContract("SelfDestructibleCosmicSignatureGame");
-	await cosmicSignatureGame.finalizeTesting();
-}
+// // const hre = require("hardhat");
+// const { getCosmicSignatureGameContract } = require("../helpers.js");
 
-main()
-	.then(() => process.exit(0))
-	.catch(error => {
-		console.error(error);
-		process.exit(1);
-	});
+// async function main() {
+// 	// let privKey = process.env.PRIVKEY;
+// 	// if (privKey == undefined || privKey.length <= 0) {
+// 	// 	console.info(
+// 	// 		// todo-1 "scripts/deploy.js" no longer exists.
+// 	// 		"Please provide private key on the command line as ENVIRONMENT variable 'PRIVKEY', example : PRIVKEY=\"0x21982349...\" npx hardhat run scripts/deploy.js",
+// 	// 	);
+// 	// 	process.exit(1);
+// 	// }
+// 	// let testingAcct = new hre.ethers.Wallet(privKey, hre.ethers.provider);
+// 	const cosmicSignatureGame = await getCosmicSignatureGameContract("SelfDestructibleCosmicSignatureGame");
+// 	await cosmicSignatureGame.finalizeTesting();
+// }
+
+// main()
+// 	.then(() => (process.exit(0)))
+// 	.catch((errorObject_) => {
+// 		console.error(errorObject_);
+// 		process.exit(1);
+// 	});
