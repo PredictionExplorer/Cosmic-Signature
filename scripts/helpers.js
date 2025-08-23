c8690e900f5a1f6c1e128660b2b785fd81bb6842
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-08-23 10:20:59 -0400

    Edited comments. Id=202509284

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,19 +1,17 @@
-// [Comment-202509229]
-// Issue. These are legacy functions that I stopped short of deleting for now.
-// Consider deleting them.
-// [/Comment-202509229]
-
 // See also: "../src/Helpers.js".
 
 "use strict";
 
 // const hre = require("hardhat");
 
-// // todo-1 This should return an object: `{cosmicSignatureGameAddress, cosmicSignatureGame,}`.
+// /// [Comment-202509229]
+// /// Issue. This is a legacy function that I stopped short of deleting for now.
+// /// Consider deleting it.
+// /// [/Comment-202509229]
+// /// todo-1 This should return an object: `{cosmicSignatureGameAddress, cosmicSignatureGame,}`.
 // async function getCosmicSignatureGameContract(cosmicSignatureGameContractName = "CosmicSignatureGame") {
 // 	const cosmicSignatureGameAddress = process.env.COSMIC_SIGNATURE_GAME_ADDRESS;
-// 	// todo-1 Unnecessary to validate length?
-// 	if (cosmicSignatureGameAddress == undefined || cosmicSignatureGameAddress.length != 42) {
+// 	if (cosmicSignatureGameAddress == undefined || cosmicSignatureGameAddress.length <= 0) {
 // 		console.error("COSMIC_SIGNATURE_GAME_ADDRESS environment variable does not contain contract address.");
 // 		process.exit(1);
 // 	}
@@ -21,9 +19,8 @@
 // 	// [Comment-202502096]
 // 	// Issue. This uses default signer. Is it a problem?
 // 	// todo-1 Everywhere we call `getContractAt`, think about the above.
-// 	// todo-1 Everywhere we call `getContractAt`, consider calling `attach`. See `deployContractsAdvanced`.
-// 	// todo-1 Also `getContractFactory`.
-// 	// todo-1 Mybe add a param: `ownerSigner`, similar to `deployerSigner` in some other functions.
+// 	// todo-1 Everywhere we call `getContractAt`, consider calling `attach` on the contract factory. See `deployContractsAdvanced`.
+// 	// todo-1 Mybe add a param: `defaultSigner`, similar to `deployerSigner` in some other functions.
 // 	// todo-1 Some scripts use the `PRIVKEY` environment variable. So the caller can pass that wallet to us.
 // 	// [/Comment-202502096]
 // 	const cosmicSignatureGame = await hre.ethers.getContractAt(cosmicSignatureGameContractName, cosmicSignatureGameAddress);
@@ -31,11 +28,11 @@
 // 	return cosmicSignatureGame;
 // }
 
-// // todo-1 This should return an object: `{bidderContractAddress, bidderContract,}`.
+// /// Comment-202509229 applies.
+// /// todo-1 This should return an object: `{bidderContractAddress, bidderContract,}`.
 // async function getBidderContract() {
 // 	const bidderContractAddress = process.env.BIDDER_CONTRACT_ADDRESS;
-// 	// todo-1 Unnecessary to validate length?
-// 	if (bidderContractAddress == undefined || bidderContractAddress.length != 42) {
+// 	if (bidderContractAddress == undefined || bidderContractAddress.length <= 0) {
 // 		console.error("BIDDER_CONTRACT_ADDRESS environment variable does not contain contract address.");
 // 		process.exit(1);
 // 	}
