3d101ecba491fe7ebf4475b1fdd567328bd0a5ec
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-08-31 23:07:28 -0400

    Edited comments. Id=202510099

--- contracts/production/CosmicSignatureGameStorage.sol
+++ contracts/production/CosmicSignatureGameStorage.sol
@@ -72,7 +72,7 @@ abstract contract CosmicSignatureGameStorage is ICosmicSignatureGameStorage {
 	/// todo-1 Recheck the Taras'es definition of endurance champion and chrono-warrior in the root readme document.
 	/// todo-1 It could exist there in a few places.
 	/// todo-1 Point at the document.
-	/// todo-1 Review relevant code in the whole project and comments and delegate some comments to the document.
+	/// todo-1 Review relevant code and comments in the whole project and delegate some comments to the document.
 	/// [Comment-202411075]
 	/// It makes no difference if they bid multiple times in a row. The durations do not get added up.
 	/// [/Comment-202411075]
