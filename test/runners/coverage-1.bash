fb04acbbc39bbd41c0e4490bc322ef8b8d931f58
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2026-08-31 10:45:33 -0400

    Improved the Hardhat Coverage script. Id=202609029

--- test/runners/coverage-1.bash
+++ test/runners/coverage-1.bash
@@ -13,30 +13,32 @@
 			read '-r' '-n' '1' '-s' '-p' 'Error 202505288. Press any key to finish.'
 			echo 1>&2
 		fi
-	else
-		# # Hardhat docs recommends setting this environment variable.
-		# # Although it doesn't necessarily make a difference for our setup, so let's not set it until something starts failing.
-		# export SOLIDITY_COVERAGE='true'
+		exit 2
+	fi
 
-		# export HACK_SUPPORT_BIGINT_TOJSON='true'
-		export HARDHAT_MODE_CODE='1'
-		# export LONG_TEST_MODE_CODE='1'
+	# # Hardhat docs recommends setting this environment variable.
+	# # Although it doesn't necessarily make a difference for our setup, so let's not set it until something starts failing.
+	# export SOLIDITY_COVERAGE='true'
 
-		# export ENABLE_HARDHAT_PREPROCESSOR='???'
-		# export ENABLE_ASSERTS='???'
-		# export ENABLE_SMTCHECKER='???'
+	# export HACK_SUPPORT_BIGINT_TOJSON='true'
+	export HARDHAT_MODE_CODE='1'
+	# export LONG_TEST_MODE_CODE='1'
 
-		# 'npx' 'hardhat' 'coverage' '--testfiles' 'test/tests-src/MainPrize.js'
-		# 'npx' 'hardhat' 'coverage' '--testfiles' 'test/tests-src/{PrizesWallet-?.js,StakingWalletCosmicSignatureNft.js,SystemManagement.js,BidStatistics.js,Bidding.js,MainPrize.js,CosmicSignatureGame-?.js,BidderContract.js,BlockTimeStamps.js}'
-		'npx' 'hardhat' 'coverage'
+	# export ENABLE_HARDHAT_PREPROCESSOR='???'
+	# export ENABLE_ASSERTS='???'
+	# export ENABLE_SMTCHECKER='???'
 
-		if [ $? -ne 0 ]; then
-			if [[ "${1}" == '-b' ]] ; then
-				echo 'Error. Hardhat Coverage failed.' 1>&2
-			else
-				read '-r' '-n' '1' '-s' '-p' 'Error. Hardhat Coverage failed. Press any key to finish.'
-				echo 1>&2
-			fi
+	# 'npx' 'hardhat' 'coverage' '--testfiles' 'test/tests-src/MainPrize.js'
+	# 'npx' 'hardhat' 'coverage' '--testfiles' 'test/tests-src/{PrizesWallet-?.js,StakingWalletCosmicSignatureNft.js,SystemManagement.js,BidStatistics.js,Bidding.js,MainPrize.js,CosmicSignatureGame-?.js,BidderContract.js,BlockTimeStamps.js}'
+	'npx' 'hardhat' 'coverage'
+
+	if [ $? -ne 0 ]; then
+		if [[ "${1}" == '-b' ]] ; then
+			echo 'Error. Hardhat Coverage failed.' 1>&2
+		else
+			read '-r' '-n' '1' '-s' '-p' 'Error. Hardhat Coverage failed. Press any key to finish.'
+			echo 1>&2
 		fi
+		exit 2
 	fi
 )
