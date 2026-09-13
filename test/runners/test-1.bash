5185f819e15b4efa97658e451ea39e469b4834f5
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2026-09-13 18:01:09 -0400

    Minor edits. Id=202609225

--- test/runners/test-1.bash
+++ test/runners/test-1.bash
@@ -16,10 +16,10 @@
 			export ENABLE_ASSERTS="${3}"
 			export ENABLE_SMTCHECKER="${4}"
 
-			# 'npx' 'hardhat' 'test' '--grep' '^CosmicSignatureGame-Misc '
+			# 'npx' 'hardhat' 'test' '--grep' '^CosmicSignatureGameV1-Misc '
 			# 'npx' 'hardhat' 'test' '--grep' '^PrizesWallet-2 Swapping to a fresh PrizesWallet after the V2 or V3 upgrade$'
 			# 'npx' 'hardhat' 'test' '--grep' '(?<!\bLong-term aggressive bidding behaves correctly)$'
-			# 'npx' 'hardhat' 'test' '--grep' '^PrizesWallet-\d |^StakingWalletCosmicSignatureNft |^CharityWallet |^SystemManagement |^BidStatistics |^Bidding |^MainPrize |^CosmicSignatureGame-\d |^BidderContract |^BlockTimeStamps '
+			# 'npx' 'hardhat' 'test' '--grep' '^PrizesWallet-\d |^StakingWalletCosmicSignatureNft |^CharityWallet |^SystemManagement |^BidStatistics |^Bidding |^MainPrize |^CosmicSignatureGameV1-Misc |^BidderContract |^BlockTimeStamps '
 			# 'npx' 'hardhat' 'test' '--grep' '^[^ ]+(?<!-Old) '
 			'npx' 'hardhat' 'test'
 
