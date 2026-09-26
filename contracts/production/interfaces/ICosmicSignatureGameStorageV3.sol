805ef041d19212c7638f408b3492707fc7610ef7
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2026-09-26 11:35:18 -0400

    Edited comments. Id=202610072

--- contracts/production/interfaces/ICosmicSignatureGameStorageV3.sol
+++ contracts/production/interfaces/ICosmicSignatureGameStorageV3.sol
@@ -4,6 +4,7 @@ pragma solidity =0.8.34;
 import { ICosmicSignatureGameStorage } from "./ICosmicSignatureGameStorage.sol";
 
 /// @dev todo-0 If this interface is going to become empty, consider deleting it.
+/// todo-0 Remember to delete it from `Cosmic-Signature-Project-File-List.odt`.
 interface ICosmicSignatureGameStorageV3 is ICosmicSignatureGameStorage {
 	// Empty.
 }
